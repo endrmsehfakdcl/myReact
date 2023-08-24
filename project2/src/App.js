bf82c5cf39a3a6259868c21571bb0a4d53c93c07
endrmsehfakdcl <toyandy0205@gmail.com>
2023-08-24 11:12:50 +0900

    230824 FindId tabmenu

--- project2/src/App.js
+++ project2/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import AccountCreate from './page/AccountCreate';
 import AccountForm from './page/AccountForm';
 import AccountTerm from './page/AccountTerm';
+import FindId from './page/FindId';
 import Footer from './page/Footer';
 import Header from './page/Header';
 import Login from './page/Login';
@@ -16,14 +17,15 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Header></Header>
+        {/* <Header></Header> */}
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/account-create" element={<AccountCreate />} />
           <Route path="/account-term" element={<AccountTerm />} />
           <Route path="/account-form" element={<AccountForm />} />
+          <Route path="/find-id" element={<FindId />} />
         </Routes>
-        <Footer></Footer>
+        {/* <Footer></Footer> */}
       </div>
     </BrowserRouter>
   );
